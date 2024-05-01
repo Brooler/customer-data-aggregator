99a96a16104b61d549e30a769a82484284abeaf1
Marian <maryaninin97@hotmail.com>
2024-05-01 14:27:55 +0300

    Added Core and Core.Tests projects. Added Tenant core model

---
+++ CustomerDataAgregator/CustomerDataAgregator.Core.Tests/Models/TenantTests.cs
@@ -0,0 +1,23 @@
+﻿using CustomerDataAgregator.Core.Models;
+using FluentAssertions;
+
+namespace CustomerDataAgregator.Core.Tests.Models;
+
+public class TenantTests
+{
+    [Test]
+    public void FinTag_ShouldReturnFirstLetterOfEachWordInOrganisationName()
+    {
+        // Arrange
+        var tenant = new Tenant
+        {
+            OrganisationName = "Customer Data Aggregator"
+        };
+
+        // Act
+        var result = tenant.FinTag;
+
+        // Assert
+        result.Should().Be("CDA");
+    }
+}
---
+++ CustomerDataAgregator/CustomerDataAgregator.Core/Models/Tenant.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomerDataAgregator.Core.Models;
+
+public class Tenant
+{
+    public long Id { get; set; }
+
+    public string OrganisationName { get; set; }
+
+    // First letter of each word in the OrganisationName
+    public string FinTag => string.Join("", OrganisationName.Split(' ').Select(w => w.First()));
+}
